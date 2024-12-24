b804e29c419242fa4d996076ee0dfe7de117a047
Mohamad Ayash <mohamadayash220@gmail.com>
2024-12-24 20:42:08 +0200

    remove unused files

--- src/contexts/sudoku/types.ts
+++
@@ -1,8 +0,0 @@
-import { Grid } from "@/types";
-
-export type ContextProps = {
-  grid: Grid;
-  setGrid: React.Dispatch<React.SetStateAction<Grid>>;
-  time: number;
-  setTime: React.Dispatch<React.SetStateAction<number>>;
-};
